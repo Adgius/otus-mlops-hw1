24b0d23bc848d2b1487b22443d12b0e12af34f05
Adgius <twinkroman98@gmail.com>
2023-08-27 23:19:43 +0500

    fix

--- clean-data.py
+++ clean-data.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser(description='credentials')
-parser.add_argument('aws_access_key_id ', type=str, help='aws_access_key_id')
+parser.add_argument('aws_access_key_id', type=str, help='aws_access_key_id')
 parser.add_argument('aws_secret_access_key', type=str, help='aws_secret_access_key')
 args = parser.parse_args()
 
