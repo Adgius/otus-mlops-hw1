f4155f7470b045282db6efc63026a92159fd15e2
Adgius <twinkroman98@gmail.com>
2023-09-30 00:01:44 +0500

    wtf

--- 4/dags/spark-job.py
+++ 4/dags/spark-job.py
@@ -6,6 +6,7 @@ from airflow.contrib.hooks.ssh_hook import SSHHook
 from airflow.contrib.operators.sftp_operator import SFTPOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.models import Variable
+from airflow.operators.bash import BashOperator
 
 
 ssh_hook = SSHHook(ssh_conn_id='cluster_ssh_connection') 
@@ -40,6 +41,11 @@ with DAG(
                 ssh_hook=ssh_hook,
                 get_pty=True)
     
-    sftp_task >> ssh_task1 >> ssh_task2
+    execute_script = BashOperator(
+        task_id="execute_script",
+        bash_command=f"ssh ubuntu@{{ ti.xcom_pull(task_ids=[\'get_masternode_ip\']) }} 'python' '/home/ubuntu/clean-data.py' '{aws_access_key_id}' {aws_secret_access_key}'",
+    )
+    
+    sftp_task >> ssh_task1 >> execute_script
 if __name__ == "__main__":
     dag.cli()
