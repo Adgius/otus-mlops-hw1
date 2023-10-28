0829b73664574ff6257a8c58791444ce7770fa27
Adgius <twinkroman98@gmail.com>
2023-10-28 16:04:13 +0500

    fix env file

--- 5/create-env.sh
+++ 5/create-env.sh
@@ -2,3 +2,4 @@ echo -e "AIRFLOW_UID=$(id -u)" >> $HOME/otus-mlops-hw1/5/.env
 echo -e "SSH_KEY=$(cat $HOME/.ssh/id_rsa.pub)" >> $HOME/otus-mlops-hw1/5/.env
 echo -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> $HOME/otus-mlops-hw1/5/.env
 echo -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> $HOME/otus-mlops-hw1/5/.env
+echo -e "MLFLOW_URL=$(curl ifconfig.me)" >> $HOME/otus-mlops-hw1/5/.env
