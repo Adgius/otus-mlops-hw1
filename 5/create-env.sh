cc38531b1c3ff0370e0215f68f84dbe98d8f05a4
Adgius <twinkroman98@gmail.com>
2023-10-28 21:57:17 +0500

    add s3 cred

--- 5/create-env.sh
+++ 5/create-env.sh
@@ -3,7 +3,7 @@ echo -e "SSH_KEY=$(cat $HOME/.ssh/id_rsa.pub)" >> $HOME/otus-mlops-hw1/5/.env
 echo -e "MLFLOW_URL=$(curl ifconfig.me)" >> $HOME/otus-mlops-hw1/5/.env
 
 echo -e [default] >> $HOME/otus-mlops-hw1/5/.data
-echo -e endpoint_url='https://storage.yandexcloud.net' >> $HOME/otus-mlops-hw1/5/.data
-echo -e region_name = 'ru-central1' >> $HOME/otus-mlops-hw1/5/.data
-echo -e aws_access_key_id = $AWS_ACCESS_KEY_ID >> $HOME/otus-mlops-hw1/5/.data
-echo -e aws_secret_access_key = $AWS_SECRET_ACCESS_KEY >> $HOME/otus-mlops-hw1/5/.data
+echo -e endpoint_url='https://storage.yandexcloud.net' >> $HOME/otus-mlops-hw1/5/.data/credentials
+echo -e region_name = 'ru-central1' >> $HOME/otus-mlops-hw1/5/.data/credentials
+echo -e aws_access_key_id = $AWS_ACCESS_KEY_ID >> $HOME/otus-mlops-hw1/5/.data/credentials
+echo -e aws_secret_access_key = $AWS_SECRET_ACCESS_KEY >> $HOME/otus-mlops-hw1/5/.data/credentials
