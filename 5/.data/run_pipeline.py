933a50f154354951e023df3da40ee2739b200d34
Adgius <twinkroman98@gmail.com>
2023-11-01 00:52:41 +0500

    is it finish?!

--- 5/.data/run_pipeline.py
+++ 5/.data/run_pipeline.py
@@ -98,12 +98,6 @@ def main(args):
 
     set_env(args)
 
-    logger.info(os.environ['MLFLOW_S3_ENDPOINT_URL'])
-    logger.info(os.environ['AWS_DEFAULT_REGION'])
-    logger.info(os.environ['AWS_ACCESS_KEY_ID'])
-    logger.info(os.environ['AWS_SECRET_ACCESS_KEY'])
-    logger.info(os.environ['MLFLOW_ARTIFACT_URI'])
-    logger.info(os.environ['PYSPARK_SUBMIT_ARGS'])
     input_bucket = args.input_bucket
     output_artifact = args.output_artifact
     val_frac = args.val_frac
