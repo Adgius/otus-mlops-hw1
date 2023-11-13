3c6a6aa50307219ffcdacb21c9e6f9a1dbd80a0c
Ибрагимов Роман Владимирович <ibragimov_r@goldapple.ru>
2023-11-13 11:33:12 +0500

    add pgvector

---
+++ Проект/pgvector/init_pgvector.sql
@@ -0,0 +1,15 @@
+-- Install the extension we just compiled
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
+/*
+For simplicity, we are directly adding the content into this table as
+a column containing text data. It could easily be a foreign key pointing to
+another table instead that has the content you want to vectorize for
+semantic search, just storing here the vectorized content in our "items" table.
+
+"768" dimensions for our vector embedding is critical - that is the
+number of dimensions our transformer embeddings model output.
+*/
+
+CREATE TABLE items (id serial PRIMARY KEY, content TEXT, embedding vector(768));
