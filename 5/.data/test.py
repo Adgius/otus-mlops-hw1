3d58c4e3e3d9ceef480df420046093cda30b9b3e
Adguis <65772479+Adguis@users.noreply.github.com>
2024-01-01 19:38:48 +0500

    add test

--- 5/.data/test.py
+++ 5/.data/test.py
@@ -1,7 +1,9 @@
 import sys
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 logger.info("Python path: {}".format(sys.executable))
 logger.info("Python version: {}".format(sys.version))
+logger.info(os.environ)
