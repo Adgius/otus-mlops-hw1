e58998426c2eec5e99a03c5daa4ed758664b737e
Adguis <65772479+Adguis@users.noreply.github.com>
2024-01-01 19:44:18 +0500

    add test

--- 5/.data/test.py
+++ 5/.data/test.py
@@ -1,9 +1,11 @@
 import sys
 import logging
 import os
+import subprocess
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 logger.info("Python path: {}".format(sys.executable))
 logger.info("Python version: {}".format(sys.version))
 logger.info(os.environ)
+logger.info("Env from cmd: {}".format(subprocess.run(["printenv"])))
