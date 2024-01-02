b2d68d8c3202fe9dcf66488d9851f8ad1ac23037
Adguis <65772479+Adguis@users.noreply.github.com>
2024-01-02 22:20:25 +0500

    add test

--- 5/dags/run_spark.py
+++ 5/dags/run_spark.py
@@ -61,7 +61,7 @@ with DAG(
 
     ssh_test2 = SSHOperator(
                 task_id="test_task2",
-                command="pwd",
+                command="echo -e printenv > test2.sh",
                 ssh_hook=ssh_hook,
                 cmd_timeout=None)
 
