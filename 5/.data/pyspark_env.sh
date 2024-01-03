5c28ca027f240bc4d9ee21b48b9dfddbf94bc48b
Adguis <65772479+Adguis@users.noreply.github.com>
2024-01-03 15:52:12 +0500

    add cluster

--- 5/.data/pyspark_env.sh
+++ 5/.data/pyspark_env.sh
@@ -1,3 +1,3 @@
 /opt/conda/bin/python -m venv pyspark_venv
 source pyspark_venv/bin/activate
-pip install pyarrow mlflow==1.27.0 urllib3==1.25.8 pyyaml venv-pack
+pip install pyarrow mlflow==1.27.0 urllib3==1.25.8 pyyaml boto3 venv-pack
